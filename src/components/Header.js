ced0583974bdfa405324f4cebec9b71887906c95
Cuitlazotlac <cuitlazotlac@gmail.com>
2021-07-18 18:16:38 -0400

    adding github link + review the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,35 @@
-import Navbar from "react-bootstrap/Navbar";
+import { Navbar, Nav, NavDropdown, Container } from "react-bootstrap";
+
 import logo from "../assets/images/logo.svg";
+import github from "../assets/images/github.png";
 
 function Header() {
   return (
     <>
-      <Navbar bg="dark" variant="dark">
-        <Navbar.Brand href="#home">
-          <img
-            alt=""
-            src={logo}
-            width="50"
-            height="50"
-            // className="d-inline-block align-top"
-          />{" "}
-          Anime Library
-          {/* <Navbar.Collapse className="justify-content-end"> */}
-            <Navbar.Text >
-              <a href="https://myanimelist.net/" target="_blank">Datesource</a>
-            </Navbar.Text>
-          {/* </Navbar.Collapse> */}
-        </Navbar.Brand>
+      <Navbar bg="dark" variant="dark" className="header">
+        <Container>
+          <Navbar.Brand href="#home">
+            <img alt="" src={logo} width="50" height="50" /> Anime Library
+          </Navbar.Brand>
+          <Navbar.Text>
+            <Navbar.Collapse className="justify-content-end">
+              <Navbar.Text>
+                <a
+                  href="https://github.com/cuitlazotlac/anime-library"
+                  target="_blank"
+                >
+                  <img
+                    alt=""
+                    src={github}
+                    className="github"
+                    width="30"
+                    height="30"
+                  />
+                </a>{" "}
+              </Navbar.Text>
+            </Navbar.Collapse>
+          </Navbar.Text>
+        </Container>
       </Navbar>
     </>
   );
